c9a94f1c0326ba3998215cc21bc302c29130ef5f
jeangondorek <jeangondorek73@gmail.com>
2026-04-15 20:49:31 -0300

    aa

--- src/main/java/org/acme/afd/analisadorlexico/Lexical.java
+++ src/main/java/org/acme/afd/analisadorlexico/Lexical.java
@@ -14,10 +14,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class Lexical {
 
-    /*
-    em resumo, lendo o afd e os tokens eu inicio a analize léxica
-    com isso e preciso retornar a fita de saída e a tabela de símbolos
-    */
     public void analyze(Automaton afd, List<String> tokens) throws IOException {
         System.out.println("\n==== Iniciando Analisador Léxico ====");
         List<String> fita = new ArrayList<>();
@@ -27,48 +23,34 @@ public class Lexical {
         int linhaAtual = 1;
 
         for (String token : tokens) {
-            // a iedia aqui é ler da lista de tokens, iniciando o token inicial , conmo eu ja tenho isso salvo no afd,. fica mais facil
             State estadoAtual = afd.getInitialState();
             
-            // depois irei consumir os caracteres desse token e avancar o estado
-            for (char symbol : token.toCharArray()) { // funcao do java q reconhece os arrays e separa eles na quebra de linha?
+            for (char symbol : token.toCharArray()) {
                 estadoAtual = getNextState(afd, estadoAtual, String.valueOf(symbol));
             }
 
-            // A palavra foi totalmente lida. Validamos se parou numa condição de sucesso.
             State estadoFinal = estadoAtual;
 
-            // FLUXO LÓGICO: "se não final EC = X"
-            // Se o estado que terminou de ler toda a palavra não for estado de "aceitação" (final=true),
-            // marcamos a ocorrência como Erro e puxamos o nó representante do Lixo ('X').
             if (!estadoFinal.isFinal()){
                 estadoFinal = getErrorState(afd);
             }
 
-            // Extai o nome do estado ('B', 'I' ou o 'X' em caso de erro)
             String nomeDoestado = estadoFinal != null ? estadoFinal.getTokenName() : "X";
 
-            // FLUXO LÓGICO: "add FITA(EC)"
-            // Empilha na Fita o nome do estado validado.
             fita.add(nomeDoestado);
 
-            // FLUXO LÓGICO: "add TS(Linha, EC, Label)"
-            // Registra as propriedades na Tabela de Símbolos
+
             tabelaSimbolos.add(SymbolTableEntry.builder()
                     .line(linhaAtual)
                     .identifier(nomeDoestado)
                     .label(token)
                     .build());
 
-            // Pula de linha 
             linhaAtual++;
         }
 
-        // FLUXO LÓGICO: "add Fita ($)"
-        // Encerramento formal do arquivo
         fita.add("$");
 
-        // ---- EXIBIÇÃO NO TERMINAL ---- //
         System.out.println("\n==== FITA DE SAÍDA ====");
         System.out.println(String.join(" ", fita));
 
@@ -85,23 +67,15 @@ public class Lexical {
     }
 
     private State getNextState(Automaton afd, State estadoAtual, String symbol){
-        // aqui eu chamo as transições
         for (Transition transition : afd.getTransitions()){
-            // com isso, posso pegar o estado atual como source "origem" que esta definida nas minhas transicoes
-            // dando sequencia, vejo se o simbolo é igual ao simbolo da transicao, com isso podendo pegar a traiscao destino "target"
             if (transition.getSource().equals(estadoAtual) && symbol.equals(transition.getSymbol())){
                 return transition.getTarget(); // retorna o estado destino
             }
         }
         
-        // Se nenhuma transição for encontrada, a letra lida quebrou a regra formando uma palavra inválida.
-        // O autômato deve então ser redirecionado para o Estado "Sumidouro" / "Lixo" (Sink State).
         return getErrorState(afd); 
     }
 
-    /**
-     * Busca no autômato a instância designada formalmente como Estado de Erro (cujo tokenName é "X").
-     */
     private State getErrorState(Automaton afd) {
         for (State s : afd.getStates()) {
             if ("X".equals(s.getTokenName())) {
